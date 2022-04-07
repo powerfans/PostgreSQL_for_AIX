612b8449cca156f272fde3959c23b59ca2168188
lsq <nikelsq@sina.com>
2022-04-07 15:55:21 +0800

    origin commit on 2022-4-7

---
+++ scripts/src/12.1/GET_SOURCE.links.sh
@@ -0,0 +1,5 @@
+##1.TAR source
+wget -c https://ftp.postgresql.org/pub/source/v12.1/postgresql-12.1.tar.gz --no-check-certificate
+
+
+
---
+++ scripts/src/12.5/GET_SOURCE.links.sh
@@ -0,0 +1,4 @@
+##1.TAR source
+wget -c https://ftp.postgresql.org/pub/source/v12.5/postgresql-12.5.tar.gz --no-check-certificate
+
+
